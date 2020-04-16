94e8e46c9b685c10d90030d11abcddbe0506e48b
ilena <ilena@LAPTOP-38G0EMP3>
2020-04-15 21:30:42 -0500

    db

---
+++ Artezanias2/Infrastructure/Artezanias2Context.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artezanias2.Infrastructure
+{
+    public class Artezanias2Context: DbContext
+    {
+
+        public Artezanias2Context(DbContextOptions<Artezanias2Context> options) : base(options)
+  
+    {
+
+    }
+    
+    
+}
